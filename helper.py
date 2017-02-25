7de652c133331b49fffa8f2a7d3b74997c03d488
Seunghun Lee <waydi1@gmail.com>
2017-02-24 19:03:45 -0800

    Add image processor and video processor

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from moviepy.editor import VideoFileClip
 
 
 def get_images(files):
@@ -62,3 +63,31 @@ def undistort_image(image, mtx, dist):
     undist = cv2.undistort(image, mtx, dist, None, mtx)
 
     return undist
+
+
+def process_image(image, mtx, dist):
+    """Process image to identify the lane boundaries.
+
+    :param image: Image to processs.
+    :param mtx: Camera matrix.
+    :param dist: Distortion coefficients.
+
+    :return: A processed image.
+    """
+    image = undistort_image(image, mtx, dist)
+    return image
+
+
+def process_video(video_file, mtx, dist, prefix="precessed_"):
+    """Process video to identify the lane boundaries.
+
+    :param video: Video file name to process.
+    :param mtx: Camera matrix.
+    :param dist: Distortino coefficients.
+    """
+    def process_fl(image):
+        return process_image(image, mtx, dist)
+    clip = VideoFileClip(video_file)
+    new_clip = clip.fl_image(process_fl)
+    new_filename = prefix + video_file
+    new_clip.write_videofile(new_filename)

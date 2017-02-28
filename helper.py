0859569296f41f13d283d25a593e70570f6e5e55
Seunghun Lee <waydi1@gmail.com>
2017-02-28 22:26:04 +0900

    Add region_of_interest

--- helper.py
+++ helper.py
@@ -127,6 +127,35 @@ def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
     return dir_binary
 
 
+def region_of_interest(img, vertices):
+    """Applies an image mask.
+
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+
+    :param image: An image to apply an image mask.
+    :param vertices: A numpy array of vertex to form the region.
+
+    :return: A masked image.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    #filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
 def process_image(image, mtx, dist):
     """Process image to identify the lane boundaries.
 

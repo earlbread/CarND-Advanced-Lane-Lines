93ed69a1f69510776aa2fe6763db7a87be8411ce
Seunghun Lee <waydi1@gmail.com>
2017-03-07 10:19:10 +0900

    Rename sobel_kernel to ksize
    sobel_kernel is too long variable name.

--- thresholding.py
+++ thresholding.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 
 
-def abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(0, 255)):
+def abs_sobel_thresh(image, orient='x', ksize=3, thresh=(0, 255)):
     """Generate a directional gradient binary image.
 
     :param image: An image to generate binary image.
     :param orient: Gradient direction, 'x' or 'y'.
-    :param sobel_kernel: Kernel size to apply sobel.
+    :param ksize: Kernel size to apply sobel.
     :param thresh: Low and high gradient threshold.
 
     :return: Directional graident binary image.
@@ -15,9 +15,9 @@ def abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(0, 255)):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
     if orient == 'x':
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
     else:
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize)
 
     abs_sobel = np.absolute(sobel)
     scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
@@ -28,18 +28,18 @@ def abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(0, 255)):
     return grad_binary
 
 
-def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+def mag_thresh(image, ksize=3, mag_thresh=(0, 255)):
     """Generate the magnitude of the gradient.
 
     :param image: An image to generate binary image.
-    :param sobel_kernel: Kernel size to apply sobel.
+    :param ksize: Kernel size to apply sobel.
     :param thresh: Low and high gradient threshold.
 
     :return: The maginitude of the gradient.
     """
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize)
     abs_sobelxy = np.sqrt(sobelx ** 2 + sobely ** 2)
     scaled_sobel = np.uint8(255*abs_sobelxy / np.max(abs_sobelxy))
     mag_binary = np.zeros_like(scaled_sobel)
@@ -47,19 +47,19 @@ def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
     return mag_binary
 
 
-def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+def dir_threshold(image, ksize=3, thresh=(0, np.pi/2)):
     """Generate the direction of the gradient.
 
     :param image: An image to generate binary image.
-    :param sobel_kernel: Kernel size to apply sobel.
+    :param ksize: Kernel size to apply sobel.
     :param thresh: Low and high gradient threshold.
 
     :return: The direction of the gradient.
     """
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-    abs_sobelx = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
-    abs_sobely = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    abs_sobelx = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize))
+    abs_sobely = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize))
     absgraddir = np.arctan2(abs_sobely, abs_sobelx)
     dir_binary = np.zeros_like(absgraddir)
     dir_binary[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
